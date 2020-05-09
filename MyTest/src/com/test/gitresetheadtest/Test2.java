c8a5ab8ba9cbab8ffa0de90a72730c276ad55e39
zhoujie <1308398245@qq.com>
2020-05-09 12:43:08 +0800

    test git reset head test1

---
+++ MyTest/src/com/test/gitresetheadtest/Test2.java
@@ -0,0 +1,14 @@
+package com.test.gitresetheadtest;
+
+/**
+ * @ClassName Test2
+ * @Description TODO
+ * @Author zhoujie
+ * @Date 2020/5/9 12:41
+ * @Version 1.0
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Test2 ==============================");
+    }
+}
